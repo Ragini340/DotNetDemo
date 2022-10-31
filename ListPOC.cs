2cffa207e217715289e94c57186ba3a5e643bdc1
Ragini340 <ragini31bxr@gmail.com>
2022-10-31 12:16:34 +0530

    Add project files.

---
+++ ListPOC.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNetDemo
+{
+    class ListPOC
+    {
+        // Show method to display the list of subject and students name
+        static void Show(IList<string> list)
+        {
+
+            // Iterating through the list
+            foreach (string str in list)
+            {
+                Console.WriteLine("\t" + str);
+            }
+        }
+
+        // Driver code from here
+        private static void Main()
+        {
+
+            // Declaring a list of subjects
+            string[] subjects = { "C", "JAVA", "HTML", "CSS",
+                          "Data Structures", "JavaScript" };
+
+            // Defining  a list
+            List<string> data = new List<string>(); //Object is created here 
+
+            // Adding students to the list
+            data.Add("Ragini");
+            data.Add("R Singh");
+            data.Add("Ragini Singh");
+
+            // Displaying subjects
+            Console.WriteLine("Subjects Name: ");
+            Show(subjects);
+
+            // Displaying students
+            Console.WriteLine("Students Name: ");
+            Show(data);
+        }
+    }
+}
+
